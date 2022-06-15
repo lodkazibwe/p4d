98dd6e3fa4bcfb0db21fc45b6e3780e4dd1824ac
lodkazibwe <lodkazibwe@gmail.com>
2022-06-15 08:51:03 +0300

    service

--- src/main/java/com/ruraaratech/p4dafrica/firebase/FirebaseInit.java
+++ src/main/java/com/ruraaratech/p4dafrica/firebase/FirebaseInit.java
@@ -15,7 +15,7 @@ public class FirebaseInit {
     @PostConstruct
     public void initialize() throws IOException {
         FileInputStream serviceAccount =
-                new FileInputStream("./service-account.json");// /home/iat/iat
+                new FileInputStream("/home/qci_user/apps/p4d/service-account.json");// /home/iat/iat
         //./service-account.json
 
         FirebaseOptions options = FirebaseOptions.builder()
