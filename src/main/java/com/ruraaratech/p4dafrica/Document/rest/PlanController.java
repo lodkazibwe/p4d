6c4f88f61e43e25145a4c4a1c67251e67805d1e7
lodkazibwe <lodkazibwe@gmail.com>
2022-06-18 18:15:57 +0300

    files

--- src/main/java/com/ruraaratech/p4dafrica/Document/rest/PlanController.java
+++ src/main/java/com/ruraaratech/p4dafrica/Document/rest/PlanController.java
@@ -7,17 +7,16 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import java.io.IOException;
 import java.util.List;
-
+@CrossOrigin
+@RestController
+@RequestMapping("/plan")
 public class PlanController {
     @Autowired
     PlanService budgetService;
