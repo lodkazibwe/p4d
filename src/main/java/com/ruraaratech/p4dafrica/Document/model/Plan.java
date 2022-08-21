dc26d2bcda6987a733dff957753b4555475009c2
lodkazibwe <lodkazibwe@gmail.com>
2022-08-21 09:57:43 +0300

    file upload

--- src/main/java/com/ruraaratech/p4dafrica/Document/model/Plan.java
+++ src/main/java/com/ruraaratech/p4dafrica/Document/model/Plan.java
@@ -19,12 +19,12 @@ public class Plan {
     private long id;
     private String title;
     private int year;
-    @Column(length=5000)
+    @Column(length=1000)
     private String file;
     private long sector;
     private long district;
     private long country;
-    @Column(length = 1000)
+    @Column(length = 50000)
     private URL url;
     private long createdBy;
     private long updatedBy;
